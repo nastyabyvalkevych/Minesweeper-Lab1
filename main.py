549a00d3899913dfa3d020085b77e65807227622
Nastya Byvalkevych <nastuly72@gmail.com>
2023-02-21 19:47:07 +0200

    created class structure and main function

--- main.py
+++ main.py
@@ -1,1 +1,64 @@
-1
+import pygame
+from pygame.locals import *
+import pygame_menu
+from pygame_menu import themes
+import random
+import math
+
+
+pygame.init()
+
+# frames per second
+fps = 120
+
+# width and height of each cell
+cell_size = 50
+
+# height of the panel containing the timer and flag count
+top_panel_height = 50
+
+# values for mouse click event
+left_mouse_click = 1
+right_mouse_click = 3
+
+# font for displaying clues, time, and flag count
+font = pygame.font.Font(pygame.font.get_default_font(), 18)
+
+
+#define colors for displaying clues
+clue_colors = ['', 'blue', 'chartreuse4', 'purple', 'red', 'green', 'turquoise', 'black']
+
+ABOUT = ['pygame-minesweeper',
+         'Authors: Nastya & Danya',
+         'Email: .....']
+
+class Game:
+    pass
+
+
+class Cell(pygame.Rect):
+    pass
+
+
+def main():
+    # create the game
+    game = Game()
+
+    # game loop
+    clock = pygame.time.Clock() #створити об’єкт, який допоможе відстежувати час
+    running = True
+    while running:
+
+        clock.tick(fps)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+
+        pygame.display.update()
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
